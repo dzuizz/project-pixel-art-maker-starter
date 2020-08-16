f6a441f33ef94beffb039a3f2a766f0c0fd21fa1
dzuizz <59071340+dzuizz@users.noreply.github.com>
2020-08-16 22:17:05 +0800

    Pixel project submission

--- designs.js
+++ designs.js
@@ -1,10 +1,34 @@
 // Select color input
+var color = document.getElementById('colorPicker').value;
 // Select size input
+var width = document.getElementById('inputWidth').value;
+var height = document.getElementById('inputHeight').value;
+const submit = document.getElementById("submitB");
 
 // When size is submitted by the user, call makeGrid()
+sizePicker.addEventListener('submit', function() {
+  event.preventDefault();
+  width = document.querySelector('#inputWidth').value;
+  height = document.querySelector('#inputHeight').value;
+  makeGrid(height, width);
+})
 
-function makeGrid() {
-
+function makeGrid(h, w) {
 // Your code goes here!
-
+  var table = document.getElementById('pixelCanvas')
+  table.innerHTML = "";
+  for (var x = 0 ; x < h ; x++) {
+    //insert row
+    var row = table.insertRow(x)
+    for (var y = 0 ; y < w ; y++) {
+      // Make cell
+      var cell = row.insertCell(y)
+      // Add 'addEventListener'
+      cell.addEventListener('click', function() {
+        const targetCell = event.target;
+        const color = document.getElementById('colorPicker').value;
+        targetCell.style.backgroundColor = color;
+      })
+    }
+  }
 }
